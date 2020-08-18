22d69ebdb35f15e53b5789faf92d2ee6e76732bb
Lady Stock <nikki.castlea@gmail.com>
2020-08-18 17:43:39 -0600

    added dollar sign to value in table

--- public/index.js
+++ public/index.js
@@ -33,7 +33,7 @@ function populateTable() {
     let tr = document.createElement("tr");
     tr.innerHTML = `
       <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
+      <td>$${transaction.value}</td>
     `;
 
     tbody.appendChild(tr);
